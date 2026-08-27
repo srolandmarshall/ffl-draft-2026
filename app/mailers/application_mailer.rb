f9f296500c43f456266b48859cbde3d42508b3ab
Marshin Monster Agent <unease-wrangle-0g@icloud.com>
2026-08-26 21:29:24 -0400

    Use verified draft sender

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch("RESEND_FROM_EMAIL", "Fantasy Draft <onboarding@resend.dev>")
+  default from: ENV.fetch("RESEND_FROM_EMAIL", "Fantasy Draft <draft@sammarshall.us>")
   layout "mailer"
 end
