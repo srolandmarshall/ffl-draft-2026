af959f1d5036a5d3fa22de41cb93aef4c7a404d6
Marshin Monster Agent <unease-wrangle-0g@icloud.com>
2026-08-26 21:45:22 -0400

    Allow assets to precompile without sender

--- config/initializers/resend.rb
+++ config/initializers/resend.rb
@@ -1,2 +1,2 @@
 Resend.api_key = -> { ENV.fetch("RESEND_API_KEY") }
-ENV.fetch("RESEND_FROM_EMAIL") if Rails.env.production?
+ENV.fetch("RESEND_FROM_EMAIL") if Rails.env.production? && ENV["SECRET_KEY_BASE_DUMMY"].blank?
